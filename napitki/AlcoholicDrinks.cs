41c1dd1c20c8d6d63ed4068c129f936a577c6b15
sashaYanushevich <rewqsasha217@gmail.com>
2022-02-27 19:46:54 +0300

    Добавьте файлы проекта.

---
+++ napitki/AlcoholicDrinks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace napitki
+{
+    abstract class AlcoholicDrinks : BottledDrinks
+    {
+        private double degreeAlcohol { get; set; }
+
+        protected AlcoholicDrinks(string name,double volume,double degreeAlcohol) : base (name,volume)
+        {
+            this.degreeAlcohol = degreeAlcohol;
+        }
+    }
+}
---
+++ napitki/BottledDrinks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace napitki
+{
+    abstract class BottledDrinks
+    {
+        private string name { get; set; }
+        private double volume { get; set; }
+
+        protected BottledDrinks(string name, double volume)
+        {
+            this.name = name;
+            this.volume = volume;
+        }
+
+    }
+}
